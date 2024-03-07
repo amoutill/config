41fc566e1b7532c8941921a9cb353c7443b6ccd2
amoutill <amoutill@student.42lehavre.fr>
2024-03-07 05:44:20 +0100

    Added nix config

---
+++ nixpkgs/darwin/configuration.nix
@@ -0,0 +1,46 @@
+{ config, pkgs, ... }:
+
+{
+  # Setting XDG dirs because macOS
+  environment.variables = {
+    XDG_CACHE_HOME = "$HOME/.cache";
+    XDG_CONFIG_HOME = "$HOME/.config";
+    XDG_DATA_HOME = "$HOME/.local/share";
+    XDG_STATE_HOME = "$HOME/.local/state";
+  };
+
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages = with pkgs; [
+    neovim
+    ripgrep
+    nerdfonts
+    norminette
+    rustup
+  ];
+
+  # Use a custom configuration.nix location.
+  # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
+  environment.darwinConfig = "$HOME/.config/nixpkgs/darwin/configuration.nix";
+
+  # Auto upgrade nix package and the daemon service.
+  services.nix-daemon.enable = true;
+  # nix.package = pkgs.nix;
+
+  # Enable sudo authentication with Touch ID
+  security.pam.enableSudoTouchIdAuth = true;
+
+  # Set nix to use XDG dirs
+  nix.extraOptions = ''
+    use-xdg-base-directories = true
+    '';
+
+  # Create /etc/zshrc that loads the nix-darwin environment.
+  programs.zsh.enable = true;  # default shell on catalina
+  programs.bash.enable = true;
+  # programs.fish.enable = true;
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 4;
+}
