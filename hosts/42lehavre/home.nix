3bcc0263af7714ce97616f05b71b6317c1b5ddca
amoutill <amoutill@student.42lehavre.fr>
2024-03-13 02:53:43 +0100

    fixed 42 home.nix

--- hosts/42lehavre/home.nix
+++ hosts/42lehavre/home.nix
@@ -1,9 +1,9 @@
 { config, pkgs, ... }:
 
 {
-  #modules = {
-  #  ../../home-manager/home.nix
-  #];
+  imports = [
+    ../../home-manager/home.nix
+  ];
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "amoutill";
